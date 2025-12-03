1b2e770e99794692c1291d61eef7b2077728fa46
Le Dao Tan Thanh <thanhldt060802@gmail.com>
2025-12-03 23:54:30 +0700

    Continue implement projects\stream-training-mini-project

--- projects/stream-training-mini-project/src/Main.java
+++ projects/stream-training-mini-project/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Map;
 
 import entity.Course;
 import entity.Enrollment;
@@ -7,38 +8,57 @@ import mockdata.MockData;
 
 public class Main {
 
+    public enum TaskName {
+        A1, A2, A3, A4, A5, A6,
+        B1, B2, B3, B4, B5, B6,
+        C1, C2, C3, C4, C5, C6,
+        D1, D2, D3, D4, D5, D6,
+        E1, E2, E3, E4, E5, E6,
+        F1, F2, F3, F4, F5, F6
+    }
+
+    private static final TaskName TASK_CHOOSEN = TaskName.A3;
+
     public static void main(String[] args) {
 
         MockData mockData = new MockData(2025L);
-
         List<Student> students = mockData.students();
         List<Course> courses = mockData.courses();
         List<Enrollment> enrollments = mockData.enrollments(students, courses);
+        Task task = new Task(students, courses, enrollments);
+        task.showData();
 
-        System.out.println("Mock data - Student:");
-        printListEntities(students);
-        System.out.println();
-        System.out.println("Mock data - Course:");
-        printListEntities(courses);
-        System.out.println();
-        System.out.println("Mock data - Enrollment:");
-        printListEntities(enrollments);
-        System.out.println();
-
-        // Triển khai các bài toán trong Task.java và kiểm thử bên dưới
-
-        // ...
-        
-    }
-
-    public static <T> void printEntity(T entity) {
-        System.out.println(entity);
-    }
-
-    public static <T> void printListEntities(List<T> entities) {
-        for (T entity : entities) {
-            printEntity(entity);
+        switch (TASK_CHOOSEN) {
+            case TaskName.A1: {
+                List<Student> result = task.A1();
+                if (result != null) {
+                    result.forEach(
+                        (student) -> System.out.println(student));
+                }
+                break;
+            }
+            case TaskName.A2: {
+                List<Student> result = task.A2();
+                if (result != null) {
+                    result.forEach(
+                        (student) -> System.out.println(student));
+                }
+                break;
+            }
+            case TaskName.A3: {
+                Map<Long, Double> result = task.A3();
+                if (result != null) {
+                    result.forEach(
+                        (studentId, studentGpa) -> System.out.println(String.format("Id=%d, GPA=%f", studentId, studentGpa)));
+                }
+                break;
+            }
+            default: {
+                System.out.println("Invalid task choosen!");
+                break;
+            }
         }
+
     }
 
 }
--- projects/stream-training-mini-project/src/Task.java
+++ projects/stream-training-mini-project/src/Task.java
@@ -1,5 +1,98 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import entity.Course;
+import entity.Enrollment;
+import entity.Student;
+
 public class Task {
 
-    
-    
+    private List<Student> students; // Danh sách sinh viên
+    private List<Course> courses; // Danh sách khoá học
+    private List<Enrollment> enrollments; // Danh sách môn học ghi nhận
+
+    public Task(List<Student> students, List<Course> courses, List<Enrollment> enrollments) {
+        this.students = students;
+        this.courses = courses;
+        this.enrollments = enrollments;
+    }
+
+    public void showData() {
+        System.out.println("Mock data - Student:");
+        this.students.forEach(
+            (student) -> System.out.println(student));
+        System.out.println();
+        System.out.println("Mock data - Course:");
+        this.courses.forEach(
+            (course) -> System.out.println(course));
+        System.out.println();
+        System.out.println("Mock data - Enrollment:");
+        this.enrollments.forEach(
+            (enrollment) -> System.out.println(enrollment));
+        System.out.println();
+    }
+
+    /*
+     * Câu A1 - Dễ:
+     * - Mô tả: Lọc sinh viên có giới tính nữ.
+     * - Trả về: List sinh viên.
+     */
+    public List<Student> A1() {
+        List<Student> result = this.students.stream()
+            .filter(
+                    (student) -> student.getGender().equals(enumeration.Gender.MALE))
+            .collect(Collectors
+                    .toList());
+
+        return result;
+    }
+
+    /*
+     * Câu A2 - Dễ:
+     * - Mô tả: Lọc sinh viên có tuổi lớn hơn 21.
+     * - Trả về: List sinh viên.
+     */
+    public List<Student> A2() {
+        List<Student> result = this.students.stream()
+            .filter(
+                    (student) -> student.getAge() > 21)
+            .collect(Collectors
+                    .toList());
+
+        return result;
+    }
+
+    /*
+     * Câu A3 - Trung bình:
+     * - Mô tả: Lọc sinh viên có điểm trung bình trên các môn học ghi nhận lớn hơn hoặc bằng 8.0.
+     * - Trả về: Map với key là mã số sinh viên, value là điểm trung bình tính được của sinh viên đó.
+     */
+    public Map<Long, Double> A3() {
+        // Map<Long, Double> result = this.enrollments.stream()
+        //     .collect(Collectors
+        //         .groupingBy(
+        //             (enrollment) -> enrollment.getStudentId()))
+        //     .entrySet()
+        //     .stream()
+        //     .collect(Collectors
+        //         .toMap(
+        //             (entry) -> entry.getKey(),
+        //             (entry) -> entry.getValue().stream()
+        //                 .mapToDouble(
+        //                     (enrollment) -> enrollment.getScore())
+        //                 .average()
+        //                 .getAsDouble()));
+
+        Map<Long, Double> result = this.enrollments.stream()
+            .collect(Collectors
+                .groupingBy(
+                    (enrollment) -> enrollment.getStudentId(),
+                    Collectors
+                        .averagingDouble(
+                            (enrollment) -> enrollment.getScore())));
+
+        return result;
+    }
+
 }
